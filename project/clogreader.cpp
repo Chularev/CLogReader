a0b654ac50bde8725f52695da117453c64437e97
Alex <chulgarev@gmail.com>
2025-07-18 18:28:22 +0700

    Started new programm

---
+++ project/clogreader.cpp
@@ -0,0 +1,29 @@
+#include "clogreader.h"
+
+CLogReader::CLogReader()
+{
+
+}
+
+CLogReader::~CLogReader()
+{
+
+}
+
+bool CLogReader::Open()
+{
+    return false;
+}
+void  CLogReader::Close()
+{
+
+}
+
+bool CLogReader::SetFilter(const char *filter)
+{
+
+}
+bool GetNextLine(char *buf,  const int bufsize); 
+{
+    
+}
