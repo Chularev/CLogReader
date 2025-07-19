4262508ae097372669fc052310d1583be38f5e1d
Alex <chulgarev@gmail.com>
2025-07-19 23:33:32 +0700

    Changed script

--- scripts/perf_stat.sh
+++ scripts/perf_stat.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR" || exit
 
 
-sudo perf stat -d ../project/VideoCompensation -pathInput ../data/input.yuv -w 1920 -h 1000 -pathOutput ../data/result.yuv
+sudo perf stat -d ../project/CLogReader ../data/testfile.txt "Jul 19 11:01:37*gdm-x-session[2215]*4?\""
